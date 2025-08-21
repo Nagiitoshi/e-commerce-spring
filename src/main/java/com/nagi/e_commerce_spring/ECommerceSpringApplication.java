b90070bdc1fdf1d018986667c8faa2d6d244cdd5
Nagiitoshi <kitsunetech6@gmail.com>
2025-08-21 19:19:59 -0300

    first commit

---
+++ src/main/java/com/nagi/e_commerce_spring/ECommerceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nagi.e_commerce_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nagi/e_commerce_spring/ECommerceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagi.e_commerce_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
